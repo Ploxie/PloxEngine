985bef7efd1700259f111aa2059f95fc9b613668
Ploxie <joakim.ekbladh@gmail.com>
2023-05-17 13:08:12 +0200

    Added HashCombine

--- PloxEngine/src/utility/Utilities.h
+++ PloxEngine/src/utility/Utilities.h
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include "eastl/string.h"
 #include "Enum.h"
 #include <malloc.h>
 
@@ -17,4 +18,11 @@
 namespace Util
 {
     void FatalExit(const char *message, int exitCode);
-}
+
+    template<class T>
+    inline void HashCombine(size_t &s, const T &v)
+    {
+	eastl::hash<T> h;
+	s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
+    }
+} // namespace Util
