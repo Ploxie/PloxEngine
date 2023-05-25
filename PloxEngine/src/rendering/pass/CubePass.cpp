309d8737ea56deed08e5de2e11551bfff371672d
Ploxie <joakim.ekbladh@gmail.com>
2023-05-26 00:21:26 +0200

    UV

--- PloxEngine/src/rendering/pass/CubePass.cpp
+++ PloxEngine/src/rendering/pass/CubePass.cpp
@@ -28,21 +28,26 @@ CubePass::CubePass(GraphicsAdapter* adapter, DescriptorSetLayout* offsetBufferSe
 
     VertexInputAttributeDescription attributeDescription[] = {
 	{ "POSITION", 0, 0, Format::R32G32B32_SFLOAT, 0 },
-	{ "COLOR", 1, 0, Format::R32G32B32A32_SFLOAT, 12 }
+	{ "NORMAL", 1, 1, Format::R32G32B32_SFLOAT, 0 }
+    };
+
+    VertexInputBindingDescription bindingDescriptions[] = {
+	{ 0, sizeof(float) * 3, VertexInputRate::VERTEX },
+	{ 1, sizeof(float) * 3, VertexInputRate::VERTEX },
     };
 
     GraphicsPipelineCreateInfo pipelineCreateInfo = {};
     GraphicsPipelineBuilder builder(pipelineCreateInfo);
     builder.SetVertexShader("assets/shaders/cube_vs");
     builder.SetFragmentShader("assets/shaders/cube_ps");
-    builder.SetVertexBindingDescription({ 0, sizeof(Vertex), VertexInputRate::VERTEX });
+    builder.SetVertexBindingDescriptions(eastl::size(bindingDescriptions), bindingDescriptions);
     builder.SetVertexAttributeDescriptions((uint32_t) eastl::size(attributeDescription), attributeDescription);
     builder.SetColorBlendAttachment(blendState);
     builder.SetDepthTest(true, false, CompareOp::GREATER_OR_EQUAL);
     builder.SetDynamicState(DynamicStateFlags::VIEWPORT_BIT | DynamicStateFlags::SCISSOR_BIT);
     builder.SetDepthStencilAttachmentFormat(Format::D32_SFLOAT);
     builder.SetColorAttachmentFormat(Format::B8G8R8A8_UNORM);
-    builder.SetPolygonModeCullMode(PolygonMode::FILL, CullModeFlags::FRONT_BIT, FrontFace::COUNTER_CLOCKWISE);
+    builder.SetPolygonModeCullMode(PolygonMode::FILL, CullModeFlags::BACK_BIT, FrontFace::COUNTER_CLOCKWISE);
 
     DescriptorSetLayoutBinding usedBinding { DescriptorType::OFFSET_CONSTANT_BUFFER, 0, 0, 1, ShaderStageFlags::ALL_STAGES };
     DescriptorSetLayoutDeclaration layoutDeclaration { offsetBufferSetLayout, 1, &usedBinding };
@@ -51,12 +56,12 @@ CubePass::CubePass(GraphicsAdapter* adapter, DescriptorSetLayout* offsetBufferSe
     m_adapter->CreateGraphicsPipeline(1, &pipelineCreateInfo, &m_pipeline);
 
     uint32_t indices[] = {
-	0, 1, 3, 3, 1, 2,
-	1, 5, 2, 2, 5, 6,
-	5, 4, 6, 6, 4, 7,
-	4, 0, 7, 7, 0, 3,
-	3, 2, 7, 7, 2, 6,
-	4, 5, 0, 0, 5, 1
+	0, 1, 3, 1, 2, 3,
+	4, 5, 7, 5, 6, 7,
+	8, 9, 11, 9, 10, 11,
+	12, 13, 15, 13, 14, 15,
+	16, 17, 19, 17, 18, 19,
+	20, 21, 23, 21, 22, 23
     };
 
     BufferCreateInfo indexBufferCreateInfo = {};
@@ -67,32 +72,154 @@ CubePass::CubePass(GraphicsAdapter* adapter, DescriptorSetLayout* offsetBufferSe
     m_adapter->CreateBuffer(indexBufferCreateInfo, MemoryPropertyFlags::HOST_VISIBLE_BIT | MemoryPropertyFlags::HOST_COHERENT_BIT, MemoryPropertyFlags::DEVICE_LOCAL_BIT, false, &m_indexBuffer);
     m_adapter->SetDebugObjectName(ObjectType::BUFFER, m_indexBuffer, "Index Buffer");
 
-    uint16_t* indicesPtr;
+    uint32_t* indicesPtr;
     m_indexBuffer->Map((void**) &indicesPtr);
     memcpy(indicesPtr, indices, indexBufferCreateInfo.Size);
 
     BufferCreateInfo vertexBufferCreateInfo = {};
     {
-	vertexBufferCreateInfo.Size	  = 8 * sizeof(Vertex);
+	vertexBufferCreateInfo.Size	  = 6 * sizeof(float) * 24;
 	vertexBufferCreateInfo.UsageFlags = BufferUsageFlags::VERTEX_BUFFER_BIT | BufferUsageFlags::TRANSFER_DST_BIT;
     }
     m_adapter->CreateBuffer(vertexBufferCreateInfo, MemoryPropertyFlags::HOST_COHERENT_BIT, MemoryPropertyFlags::DEVICE_LOCAL_BIT, false, &m_vertexBuffer);
     m_adapter->SetDebugObjectName(ObjectType::BUFFER, m_vertexBuffer, "Vertex Buffer");
 
-    Vertex vertices[] = {
-	{ { -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
-	{ { 0.5f, -0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
-	{ { 0.5f, 0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f, 1.0f } },
-	{ { -0.5f, 0.5f, -0.5f }, { 1.0f, 1.0f, 1.0f, 1.0f } },
-	{ { -0.5f, -0.5f, 0.5f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
-	{ { 0.5f, -0.5f, 0.5f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
-	{ { 0.5f, 0.5f, 0.5f }, { 0.0f, 0.0f, 1.0f, 1.0f } },
-	{ { -0.5f, 0.5f, 0.5f }, { 1.0f, 1.0f, 1.0f, 1.0f } },
+    glm::vec3 positions[48] = {
+	{ -0.5f, -0.5f, 0.5f }, // Front
+	{ 0.5f, -0.5f, 0.5f },
+	{ 0.5f, 0.5f, 0.5f },
+	{ -0.5f, 0.5f, 0.5f },
+
+	{ 0.5f, -0.5f, -0.5f }, // Back
+	{ -0.5f, -0.5f, -0.5f },
+	{ -0.5f, 0.5f, -0.5f },
+	{ 0.5f, 0.5f, -0.5f },
+
+	{ -0.5f, -0.5f, -0.5f }, // Up
+	{ 0.5f, -0.5f, -0.5f },
+	{ 0.5f, -0.5f, 0.5f },
+	{ -0.5f, -0.5f, 0.5f },
+
+	{ -0.5f, 0.5f, 0.5f }, // Down
+	{ 0.5f, 0.5f, 0.5f },
+	{ 0.5f, 0.5f, -0.5f },
+	{ -0.5f, 0.5f, -0.5f },
+
+	{ -0.5f, 0.5f, 0.5f }, // Left
+	{ -0.5f, 0.5f, -0.5f },
+	{ -0.5f, -0.5f, -0.5f },
+	{ -0.5f, -0.5f, 0.5f },
+
+	{ 0.5f, 0.5f, -0.5f }, // Right
+	{ 0.5f, 0.5f, 0.5f },
+	{ 0.5f, -0.5f, 0.5f },
+	{ 0.5f, -0.5f, -0.5f },
+
+	{ 0, 0, 1 }, // Front
+	{ 0, 0, 1 },
+	{ 0, 0, 1 },
+	{ 0, 0, 1 },
+
+	{ 0, 0, -1 }, // Back
+	{ 0, 0, -1 },
+	{ 0, 0, -1 },
+	{ 0, 0, -1 },
+
+	{ 0, -1, 0 }, // Down
+	{ 0, -1, 0 },
+	{ 0, -1, 0 },
+	{ 0, -1, 0 },
+
+	{ 0, 1, 0 }, // Up
+	{ 0, 1, 0 },
+	{ 0, 1, 0 },
+	{ 0, 1, 0 },
+
+	{ -1, 0, 0 }, // Left
+	{ -1, 0, 0 },
+	{ -1, 0, 0 },
+	{ -1, 0, 0 },
+
+	{ 1, 0, 0 }, // Right
+	{ 1, 0, 0 },
+	{ 1, 0, 0 },
+	{ 1, 0, 0 },
     };
 
-    Vertex* verticesPtr;
+    float vertices[] = {
+	// Vertices
+	-0.5f,
+	0.5f,
+	-0.5f,
+
+	0.5f,
+	0.5f,
+	-0.5f,
+
+	0.5f,
+	0.5f,
+	0.5f,
+
+	-0.5f,
+	0.5f,
+	0.5f,
+
+	-0.5f,
+	-0.5f,
+	-0.5f,
+
+	0.5f,
+	-0.5f,
+	-0.5f,
+
+	0.5f,
+	-0.5f,
+	0.5f,
+
+	-0.5f,
+	-0.5f,
+	0.5f,
+
+	// Normals
+	0.0f,
+	0.0f,
+	1.0f,
+
+	0.0f,
+	0.0f,
+	1.0f,
+
+	0.0f,
+	0.0f,
+	1.0f,
+
+	-1.0f,
+	0.0f,
+	0.0f,
+
+	0.0f,
+	1.0f,
+	0.0f,
+
+	0.0f,
+	-1.0f,
+	0.0f,
+
+	0.0f,
+	-1.0f,
+	0.0f,
+
+	0.0f,
+	-1.0f,
+	0.0f,
+    };
+
+    float* verticesPtr;
     m_vertexBuffer->Map((void**) &verticesPtr);
-    memcpy(verticesPtr, vertices, 8 * sizeof(Vertex));
+    size_t size = 24 * sizeof(float) * 6;
+
+    memcpy(verticesPtr, positions, size);
+    //memcpy(verticesPtr + size, normals, size);
 }
 
 CubePass::~CubePass()
@@ -123,7 +250,10 @@ void CubePass::Record(RenderGraph* renderGraph, const CubePass::Data& data)
 
 	Constants consts = {};
 
-	glm::quat rot	= glm::quat(glm::angleAxis(m_rotation, glm::vec3(0.0f, 1.0f, 0.0f)));
+	glm::quat tmp	= glm::quat(glm::angleAxis(0.785398163f, glm::vec3(0.0f, 0.0f, 1.0f)));
+	glm::quat tmp2	= glm::quat(glm::angleAxis(0.785398163f, glm::vec3(1.0f, 0.0f, .0f)));
+	glm::quat tmp3	= glm::quat(glm::angleAxis(m_rotation, glm::vec3(0.0f, 1.0f, 0.0f)));
+	glm::quat rot	= glm::normalize(tmp3 * tmp2 * tmp);
 	glm::mat4 model = glm::mat4_cast(rot);
 
 	memcpy(consts.modelMatrix, &model[0], 16 * sizeof(float));
@@ -150,11 +280,14 @@ void CubePass::Record(RenderGraph* renderGraph, const CubePass::Data& data)
 	    Rect scissor { { 0, 0 }, { data.FrameWidth, data.FrameHeight } };
 	    command->SetScissors(0, 1, &scissor);
 
+	    Buffer* vertexBuffers[] = { m_vertexBuffer, m_vertexBuffer };
+
 	    uint32_t offset = 0; //36 * sizeof(float);
 	    command->BindDescriptorSets(m_pipeline, 0, 1, &data.OffsetBufferSet, 1, &offset);
+
 	    command->BindIndexBuffer(m_indexBuffer, 0, IndexType::UINT32);
-	    uint64_t offsets[2] = { 0, 8 * sizeof(Vertex) };
-	    command->BindVertexBuffers(0, 1, &m_vertexBuffer, offsets);
+	    uint64_t offsets[2] = { 0, 288 };
+	    command->BindVertexBuffers(0, 2, vertexBuffers, offsets);
 	    command->DrawIndexed(36, 1, 0, 0, 0);
 	}
 	command->EndRenderPass();
